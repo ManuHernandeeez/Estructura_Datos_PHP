f629d7f42c871a46703981148c10a24d9945ccef
Manuel Hernandez <manu.hernandez.15.2002@gmail.com>
2025-07-29 18:02:52 -0600

    Update 6-StrongPassword.php

---
+++ Ejercicios/6-StrongPassword.php
@@ -0,0 +1,74 @@
+<?php
+
+function minimunNumber($n, $password)
+{
+
+    $numbers = "0123456789";
+    $lower_case = "abcdefghijklmnopqrstuvwxyz";
+    $upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $special_characters = "!@#$%^&*()-+";
+
+    $missing_types = 0;
+
+
+    //Verificar si faltan digitos
+    $has_digit = false;
+
+    foreach (str_split($numbers) as $char) {
+        if (strpos($password, $char) !== false) {
+            $has_digit = true;
+            break;
+        }
+    }
+
+    if (!$has_digit) {
+        $missing_types++;
+    }
+
+    //verificar si faltan letras minus
+    $has_lower = false;
+
+    foreach (str_split($lower_case) as $char) {
+        if (strpos($password, $char) !== false) {
+            $has_lower = true;
+            break;
+        }
+    }
+
+    if (!$has_lower) {
+        $missing_types++;
+    }
+
+    //verificar si falta una letra mayus
+    $has_upper = false;
+    foreach (str_split($upper_case) as $char) {
+        if (strpos($password, $char) !== false) {
+            $has_upper = true;
+            break;
+        }
+    }
+    if (!$has_upper) {
+        $missing_types++;
+    }
+
+    //verificar si falta un caracter
+    $has_special = false;
+    foreach (str_split($special_characters) as $char) {
+        if (strpos($password, $char) !== false) {
+            $has_special = true;
+            break;
+        }
+    }
+    if (!$has_special) {
+        $missing_types++;
+    }
+
+
+    //
+    return max($missing_types, 6 - $n);
+}
+
+//
+
+$result1 = minimunNumber($n1, $password1);
+echo "La contraseña'" . $password1 . "', necesita añadir: " . $result1 . " caracteres.\n"; 

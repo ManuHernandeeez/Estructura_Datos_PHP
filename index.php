fb522ed034426614bf04d55d007e6bfc1347f4e6
Manuel Hernandez <manu.hernandez.15.2002@gmail.com>
2025-07-29 18:37:23 -0600

    Listado de ejercicios

--- index.php
+++ index.php
@@ -11,9 +11,15 @@
     <h1>Ejercicios</h1>
 
     <ul>
-        <li><a href="Ejercicios\6-StrongPassword.php">Ejercicio 6</li>
-
-
+        <li><a href="Ejercicios/1-HeightChecker.php">Ejercicio 1: HeightChecker</a></li>
+        <li><a href="Ejercicios/2-InsertionSort.php">Ejercicio 2: InsertionSort</a></li>
+        <li><a href="Ejercicios/3-BinarySearch.php">Ejercicio 3: BinarySearch</a></li>
+        <li><a href="Ejercicios/4-HappyNumber.php">Ejercicio 4: HappyNumber</a></li>
+        <li><a href="Ejercicios/5-BirthdayCakeCandles.php">Ejercicio 5: BirthdayCakeCandles</a></li>
+        <li><a href="Ejercicios/6-StrongPassword.php">Ejercicio 6: StrongPassword</a></li>
+        <li><a href="Ejercicios/7-MarsExploration.php">Ejercicio 7: MarsExploration</a></li>
+        <li><a href="Ejercicios/8-MergeSort.php">Ejercicio 8: MergeSort</a></li>
+        <li><a href="Ejercicios/9-AVeryBigSum.php">Ejercicio 9: AVeryBigSum</a></li>
     </ul>
 </body>
 
